faca6364507f962179a75474ebb0f14fabd6262e
nguyendev2018 <nguyendev2018@gmail.con>
2021-12-23 11:04:15 +0700

    --- Auto-Git Commit ---

---
+++ js/api/axiosClient.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+const axiosClient = axios.create({
+    baseURL: "https://js-post-api.herokuapp.com/api/",
+    headers: {
+        'Content-Type': "application/json",
+
+    }
+})
+export default axiosClient;
--- js/api/post.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const axiosClient = axios.create({
-    baseURL: "https://post-ui-lilac.vercel.app/",
-    headers: {
-        'Content-Type': "application/json",
-
-    }
-})
-export default axiosClient;
