6f692f447c1b0ec02b2c88977f94d010abbe60f0
nguyendev2018 <nguyendev2018@gmail.con>
2021-12-24 09:38:55 +0700

    --- Auto-Git Commit ---

--- js/api/post.js
+++ js/api/post.js
@@ -1,4 +1,12 @@
-const post = {
+import axiosClient from "./axiosClient";
 
+const post = {
+    getAll(params) {
+        const url = './posts';
+        return axiosClient.get(url, { params });
+    },
+    getById(id) {
+
+    }
 }
 export default post
