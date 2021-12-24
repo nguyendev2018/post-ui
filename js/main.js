6e018dae89b91388baa5f8cff31e0660647d1f80
nguyendev2018 <nguyendev2018@gmail.con>
2021-12-24 10:08:57 +0700

    --- Auto-Git Commit ---

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 import axiosClient from './api/axiosClient';
+import post from './api/post';
 async function main() {
-    const response = axiosClient.get("/posts");
+    const queryParams = {
+        _page: 1,
+        _limit: 5,
+    }
+    const response = await post.getAll(queryParams);
     console.log(response);
 }
 main();
