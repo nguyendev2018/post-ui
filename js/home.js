e3df3f5cd81e3864f910845a71dfa3c9ee117441
nguyendev2018 <nguyendev2018@gmail.con>
2021-12-28 15:27:42 +0700

    --- Auto-Git Commit ---

--- js/home.js
+++ js/home.js
@@ -1,18 +1,23 @@
 import post from './api/post';
 
+function createPostItem() {
+
+}
+
 function renderPost(postList) {
     console.log({ postList });
 }
 (async() => {
-    try {
-        const queryParams = {
-            _page: 1,
-            _limit: 6,
-        }
-        const data = await post.getAll(queryParams);
-        renderPost(data)
-    } catch (error) {
-        console.log('get all failed');
-    }
+        try {
+            const queryParams = {
+                _page: 1,
+                _limit: 6,
+            }
+            const { data, pagination } = await post.getAll(queryParams);
+        })
+}
+catch (error) {
+    console.log('get all failed');
+}
 
 })()
