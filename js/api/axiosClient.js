a862a0f501c38fa2bffeac8db7685f5c1971587a
nguyendev2018 <nguyendev2018@gmail.con>
2021-12-28 13:57:25 +0700

    --- Auto-Git Commit ---

--- js/api/axiosClient.js
+++ js/api/axiosClient.js
@@ -1,30 +1,33 @@
 import axios from "axios";
 const axiosClient = axios.create({
-    baseURL: "https://js-post-api.herokuapp.com/api/",
-    headers: {
-        'Content-Type': "application/json",
+        baseURL: "https://js-post-api.herokuapp.com/api/",
+        headers: {
+            'Content-Type': "application/json",
 
-    }
-})
-// Add a request interceptor
-axiosClient.interceptors.request.use(function (config) {
+        }
+    })
+    // Add a request interceptor
+axiosClient.interceptors.request.use(function(config) {
     // Do something before request is sent
     // config là config request
     console.log('request interceptors', config);
     // Attach token to request if exits
-
+    const accessToken = localStorage.getItem("access_token")
+    if (accessToken) {
+        config.headers.Authorization = `Bears ${accessToken}`
+    }
     return config;
-}, function (error) {
+}, function(error) {
     // Do something with request error
     return Promise.reject(error);
 });
 
 // Add a response interceptor
-axiosClient.interceptors.response.use(function (response) {
+axiosClient.interceptors.response.use(function(response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response;
-}, function (error) {
+}, function(error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     return Promise.reject(error);
