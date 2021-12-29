2fabecaafca2a21817a758681d18cc18fc2d0066
nguyendev2018 <nguyendev2018@gmail.con>
2021-12-29 22:01:57 +0700

    --- Auto-Git Commit ---

--- js/home.js
+++ js/home.js
@@ -2,14 +2,19 @@ import post from './api/post';
 
 function createPostItem(post) {
     const postElement = document.getElementById("postTemplate");
+
     const liElement = postElement.content.firstElementChild.cloneNode(true);
+
     // update title , description, author,thumbnail
     const titleElement = liElement.querySelector('[data-id="title"]')
     titleElement.textContent = post.title;
+    return liElement;
+
 }
 
 function renderPost(postList) {
     const ulElement = document.getElementById("postList");
+    if (!ulElement) return
     postList.forEach((post) => {
         const liElement = createPostItem(post);
         ulElement.appendChild(liElement)
